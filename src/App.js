9d2b31e94fe95b503c45642f24e539adf9b35866
Angie Nguyen <angie.ta.nguyen@gmail.com>
2022-08-03 18:03:35 -0700

    Uses Header, Game, GameOver components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState} from "react";
+import Header from "./components/Header";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+import * as CardModule from "./helpers/Card";
+import "./styles/App.css";
+
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [cards, setCards] = useState(CardModule.cards);
+  const [isGameOver, setIsGameOver] = useState(false);
+
   return (
     <div className="App">
-
+      <Header currentScore={currentScore} highScore={highScore}></Header>
+      <Game cards={cards} setCards={setCards} currentScore={currentScore} setCurrentScore={setCurrentScore} highScore={highScore} setHighScore={setHighScore} isGameOver={isGameOver} setIsGameOver={setIsGameOver}></Game>
+      <GameOver cards={cards} currentScore={currentScore} setCurrentScore={setCurrentScore} isGameOver={isGameOver} setIsGameOver={setIsGameOver}></GameOver>
     </div>
   );
 }
